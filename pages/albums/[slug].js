85aa9962d8f5e25e8bdac2cfb68a0ecf7abc979b
Elias Frigård <elias.frigard@tvtools.fi>
2023-11-27 18:48:56 +0200

    image modal

--- pages/albums/[slug].js
+++ pages/albums/[slug].js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
 
 import '../../app/globals.css'
 import Image from "next/image"
@@ -19,31 +20,49 @@ const imageLoader = ({ src, width, quality }) => {
   return `https:${modifiedSrc}?h=900&w=1200&q=25`
 }
 
+const fullImageLoader = ({ src, width, quality }) => {
+  let modifiedSrc = src.replace("downloads", "images");
+
+  return `https:${modifiedSrc}?q=75`
+}
+
 export default function ScorePage({ album, slug }) {
-  console.log('🚀 || file: [slug].js:23 || ScorePage || album:', album)
   const [animate, setAnimate] = React.useState(false)
+  const [ratio, setRatio] = React.useState('1/1')
+  const [selectedImage, setSelectedImage] = React.useState(album.fields.images[0])
+
+  const [open, setOpen] = React.useState(false)
+
+  const handleImageOpen = (imageIndex) => {
+    setSelectedImage(album.fields.images[imageIndex])
+    setOpen(true)
+  }
+
+  const handleCloseModal = (imageIndex) => {
+    setOpen(false)
+  }
 
   useEffect(() => {
     setAnimate(true)
   }, [])
 
   return (
-    <div className='container w-full min-h-screen flex flex-col gap-4 items-center py-14'>
-      <h1 className='text-6xl font-extrabold tracking-wide'>
+    <div className='container text-center w-full min-h-screen flex flex-col gap-4 items-center py-14'>
+      <h1 className='text-6xl font-extrabold tracking-wide leading-[4.5rem]'>
         {album.fields.title}
       </h1>
 
-      <div className='flex gap-6 flex-wrap'>
+      <div className='flex gap-6 flex-wrap text-center items-center justify-center'>
         <div className='flex justify-center items-center gap-3'>
           <BsCalendar2CheckFill className='text-black' />
-          <Moment format='D MMMM YYYY' className='tracking-wider font-bold'>
+          <Moment format='D MMMM YYYY' className='tracking-wider font-medium'>
             {album.fields.date}
           </Moment>
         </div>
 
         <div className='flex justify-center items-center gap-3'>
           <BsPinMapFill className='text-black' />
-          <p className='tracking-wider font-bold'>
+          <p className='tracking-wider font-medium'>
             {album.fields.location}
           </p>
         </div>
@@ -57,7 +76,7 @@ export default function ScorePage({ album, slug }) {
             const classes = `relative w-full aspect-3/4 rounded-lg overflow-hidden shadow-lg duration-[600ms] ${animate ? 'opacity-100' : 'opacity-0'} ease-in`
 
             return (
-              <div key={image.sys.id} className={classes} style={{ transitionDelay: delay }} >
+              <div onClick={() => handleImageOpen(index)} key={image.sys.id} className={classes} style={{ transitionDelay: delay }} >
                 <Image
                   loader={imageLoader}
                   alt={image.fields.title}
@@ -73,6 +92,29 @@ export default function ScorePage({ album, slug }) {
           })
         }
       </div>
+
+      <Dialog
+        open={open}
+        onClose={handleCloseModal}
+        className="relative z-50"
+      >
+        <div className="fixed inset-0 bg-black/30 backdrop-blur-sm" aria-hidden="true" />
+
+        <div className="fixed inset-0 flex w-screen items-center justify-center py-12 px-24">
+          <Dialog.Panel className="rounded-lg relative h-full overflow-hidden shadow-2xl" style={{ aspectRatio: ratio }}>
+            <Image
+              alt="nature"
+              fill
+              loader={fullImageLoader}
+              className="object-fit h-[48rem] w-full object-center"
+              src={selectedImage.fields.file.url}
+              onLoad={(e) => {
+                setRatio(e.target.naturalWidth / e.target.naturalHeight)
+              }}
+            />
+          </Dialog.Panel>
+        </div>
+      </Dialog>
     </div >
   )
 }
